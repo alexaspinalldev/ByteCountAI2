aa7f0e9de4e1acf1aecc3da42ea5fba1903bdf72
Alex Aspinall <alex.aspinall@blueyonder.co.uk>
2025-04-09 15:32:08 +0100

    Build meals and user DB schema

--- bytecountai/src/db/schema/schema.ts
+++ bytecountai/src/db/schema/schema.ts
@@ -1,15 +1,35 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, timestamp, jsonb } from "drizzle-orm/pg-core";
+import { relations } from 'drizzle-orm';
 
-// User
-export const usersTable = pgTable("users", {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+// Users
+export const users = pgTable("users", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity().notNull(),
     name: varchar({ length: 255 }).notNull(),
-    age: integer().notNull(),
     email: varchar({ length: 255 }).notNull().unique(),
-});
+    // Temporary fields
+}).enableRLS();
 
-
-// Days
-
+export const usersMealsRelations = relations(users, ({ many }) => ({
+    meals: many(meals),
+}));
 
 // Meals
+export const meals = pgTable("meals", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    totalCalories: integer().notNull(),
+    mealBody: jsonb().notNull(),
+    label: varchar({ length: 255 }).notNull(),
+    userId: integer("user_id").notNull().references(() => users.id, { onDelete: "cascade" }),
+    dateTimeAdded: timestamp(({ precision: 2, withTimezone: true })).defaultNow().notNull(),
+    lastEdited: timestamp(({ precision: 2, withTimezone: true })),
+}).enableRLS();
+
+export const mealsUsersRelations = relations(meals, ({ one }) => ({
+    user: one(users, {
+        fields: [meals.userId],
+        references: [users.id],
+    }),
+}));
+
+
+// Recipes
