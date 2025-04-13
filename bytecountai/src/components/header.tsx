d872be903c2f71458904682efbe6692583330fa0
Alex Aspinall <alex.aspinall@blueyonder.co.uk>
2025-04-13 21:36:57 +0100

    Colour header icons

--- bytecountai/src/components/header.tsx
+++ bytecountai/src/components/header.tsx
@@ -3,7 +3,7 @@ import { Menu } from 'lucide-react';
 
 export default function Header() {
     return (
-        <header className="flex justify-end items-center w-full p-4">
+        <header className="flex justify-end items-center w-full p-4 text-primary">
             {/* <h1 className="text-2xl font-bold">ByteCount</h1> */}
             <div className="flex items-center space-x-4">
                 <CircleUserRound className="w-6 h-6" />
