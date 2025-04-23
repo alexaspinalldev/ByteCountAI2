121596b8e8c7c4ee20b022cd5e08c8d04b236da8
Alex Aspinall <alex.aspinall@blueyonder.co.uk>
2025-04-23 14:38:48 +0100

    Fix: Resolve issue where flex items could not shrink smaller than the explicitly set child

--- bytecountai/src/app/components/common/ui/resizeableScrollArea.tsx
+++ bytecountai/src/app/components/common/ui/resizeableScrollArea.tsx
@@ -3,33 +3,34 @@ import { ScrollArea } from "./scroll-area";
 import { useState, useEffect, useRef } from "react";
 
 export default function ResizableScrollArea({ children }: { children: React.ReactNode }) {
-
-    // * Set the height of the scroll area on window resize or mount
-    const [scrollAreaHeight, setScrollAreaHeight] = useState(0);
     const scrollAreaContainer = useRef<HTMLDivElement | null>(null);
     const scrollArea = useRef<HTMLDivElement | null>(null);
+
     useEffect(() => {
         const updateHeight = () => {
             const newHeight = scrollAreaContainer?.current!.clientHeight;
-            setScrollAreaHeight(newHeight); // Update state with the new height
+            if (scrollArea.current) {
+                scrollArea.current.style.height = `${newHeight}px`; // Set the height of the scroll area
+            }
         }
         updateHeight()
-        // Update height on window resize
-        window.addEventListener("resize", updateHeight);
+        // Update height on window resize - initialise the height back to 0 to allow for the parent to flex smaller than the child
+        window.addEventListener("resize", () => {
+            if (scrollAreaContainer.current) {
+                scrollAreaContainer.current.style.height = `0px`;
+            }
+            updateHeight()
+        });
+
         // Cleanup event listener on unmount
         return () => window.removeEventListener("resize", updateHeight);
-    }, []);
-    // scrollAreaContainer.current.clientHeight = scrollAreaHeight;
-    console.log(scrollAreaHeight)
-    // ! This works better now for initial render but cannot handle window resizing due to how we're inbjecting the height as inline style
-    // ! Needs to be refactored to use a component that wraps around children
-    // ! Needs to use a resize observer to handle resizing
+    }, []
+    );
 
     return (
         <div className="grow" ref={scrollAreaContainer}>
             {/* ^Used to explicity set the height of the ScrollArea */}
-            {/* // TODO: Make your expanding scrollarea a component you wrap around children */}
-            <ScrollArea ref={scrollArea} id="mealPadUl" className="py-2 bg-gray-100 dark:bg-zinc-900" style={{ height: `${scrollAreaHeight}px` }}>
+            <ScrollArea ref={scrollArea} id="mealPadUl" className="py-2 bg-gray-100 dark:bg-zinc-900">
                 {children}
             </ScrollArea >
         </div>
